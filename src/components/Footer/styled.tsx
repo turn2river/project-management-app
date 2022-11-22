fb9b2bddfd3a0cabc4606e79d46d94674189cc9e
Farber Denis <42491613+Bombamuerta@users.noreply.github.com>
2022-11-22 13:27:35 +0300

    feat: contacts popup added (#10)

--- src/components/Footer/styled.tsx
+++ src/components/Footer/styled.tsx
@@ -21,5 +21,25 @@ export const Contacts = styled.div`
   font-size: 15px;
   font-weight: 700;
   cursor: pointer;
+  position: relative;
+`
+
+export const ContactsPopup = styled.div`
+  background: #FCFCFD;
+  border: 1px solid rgba(102, 112, 133, 0.5);
+  border-radius: 8px;
+  position: absolute;
+  top: -110px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px 10px;
+`
+
+export const ContacLink = styled.a`
+  margin: 5px 0;
+
+  &:hover {
+    color: #000;
+  } 
 `
 
