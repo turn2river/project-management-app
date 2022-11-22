4549d5c1b70a3d332f7f825d8d1b922ba4dd21b6
Farber Denis <dsnakez11@yandex.ru>
2022-11-22 12:24:16 +0300

    feat: contacts popup added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,50 @@
+import { useState } from 'react'
+
 import {
   FooterWrapper,
   RSLink,
   Contacts,
+  ContactsPopup,
+  ContacLink
 } from './styled'
 
-export const Footer = () => (
-  <FooterWrapper>
-    <RSLink
-      href='https://rs.school/'
-      target='_blank'
-    >rs school</RSLink>
-    <Contacts>
-      MONKEY TEAM ©2022
-    </Contacts>
-  </FooterWrapper>
-)
+export const Footer = () => {
+
+  const [showPopup, setShowPopup] = useState(false)
+
+  const toggleContacts = () => {
+    setShowPopup(!showPopup)
+  }
+
+  return (
+    <FooterWrapper>
+      <RSLink
+        href='https://rs.school/'
+        target='_blank'
+      >rs school</RSLink>
+      <Contacts onClick={toggleContacts}>
+        MONKEY TEAM ©2022
+
+        { showPopup && (
+          <ContactsPopup>
+            <ContacLink
+              href="https://github.com/turn2river"
+              target='_blank'
+            >Alexey Krylov
+            </ContacLink>
+            <ContacLink
+              href="https://github.com/PavelTitov16"
+              target='_blank'
+            >Pavel Tsitou
+            </ContacLink>
+            <ContacLink
+              href='https://github.com/Bombamuerta'
+              target='_blank'
+            >Denis Farber</ContacLink>
+          </ContactsPopup>
+        )}
+
+      </Contacts>
+    </FooterWrapper>
+  )
+}
