62db11ec03e542c422b06cfc411d632c40d6584a
PavelTitov16 <pashoktitov16@gmail.com>
2022-11-23 13:09:32 +0300

    feat: add route to main page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -9,6 +9,7 @@ import { GlobalStyle } from '../../globalStyles'
 import { Header } from '../Header'
 import { Footer } from '../Footer'
 import { WellcomePage } from '../WellcomePage'
+import { MainPage } from '../MainPage'
 
 export const App = () => {
   return (
@@ -20,6 +21,10 @@ export const App = () => {
           <Route
             path="/"
             element={<WellcomePage />}
+          />
+          <Route
+            path="/main"
+            element={<MainPage />}
           />
         </Routes>
         <Footer />
---
+++ src/components/MainPage/index.tsx
@@ -0,0 +1,19 @@
+import {
+  PageContainer,
+  MainTitle,
+  Header,
+  HeaderColor,
+} from './styled'
+
+export const MainPage = () => (
+  <PageContainer>
+    <MainTitle>
+      <Header>
+            WELCOME TO
+      </Header>
+      <HeaderColor>
+            YOUR BOARDS
+      </HeaderColor>
+    </MainTitle>
+  </PageContainer>
+)
