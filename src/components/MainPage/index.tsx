f2608c292728ce3217345812c8daa8aaccab6314
Farber Denis <42491613+Bombamuerta@users.noreply.github.com>
2022-11-25 16:18:00 +0300

    Modal component (#17)
    * feat: modal component added
    
    * fix: min fix

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import { Button } from '../Button'
+import { Modal } from '../Modal'
 
 import {
   PageContainer,
@@ -18,34 +21,46 @@ import {
   DescriptionHead
 } from '../WellcomePage/styled'
 
-export const MainPage = () => (
-  <PageContainer>
-    <MainTitle>
-      <Logo />
-      <Header>
+export const MainPage = () => {
+  const [openModal, setOpenModal] = useState(false)
+  const toggleModal = () => setOpenModal(!openModal)
+
+  return (
+    <PageContainer>
+      <Modal
+        isOpen={openModal}
+        toggleModal={toggleModal}
+      >
+        <span>сюда вставляем нужную форму...
+          короче все впизду меня все заебало.</span>
+      </Modal>
+      <MainTitle>
+        <Logo />
+        <Header>
         Hey,
-      </Header>
-      <HeaderColor>
+        </Header>
+        <HeaderColor>
         username
-      </HeaderColor>
-    </MainTitle>
-    <DescriptionHead>SElect oR create a project</DescriptionHead>
-    <ProjectsBlock>
-      <ProjectCard>
-        <CardHeader>
-          <ProjectName>this is project name</ProjectName>
-          <ProjectOptions />
-        </CardHeader>
-        <ProjectDesc>
+        </HeaderColor>
+      </MainTitle>
+      <DescriptionHead>SElect oR create a project</DescriptionHead>
+      <ProjectsBlock>
+        <ProjectCard>
+          <CardHeader>
+            <ProjectName>this is project name</ProjectName>
+            <ProjectOptions />
+          </CardHeader>
+          <ProjectDesc>
          this is project description
          Lorem, ipsum dolor sit amet
          consectetur adipisicing elit. Distinctio, totam?
-        </ProjectDesc>
-      </ProjectCard>
-      <Button
-        text='+ project'
-        handleClick={() => {}}
-      />
-    </ProjectsBlock>
-  </PageContainer>
-)
+          </ProjectDesc>
+        </ProjectCard>
+        <Button
+          text='+ project'
+          handleClick={toggleModal}
+        />
+      </ProjectsBlock>
+    </PageContainer>
+  )
+}
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+
+import {
+  ModalContainer,
+  ModalWindow,
+  ModalCloseBtn,
+} from './styled'
+
+type TModal = {
+  isOpen: boolean,
+  children: ReactNode,
+  toggleModal: () => void,
+}
+
+// используем компонент Modal как обертку в нужном месте
+// клик со стейтом открытия обрабатываем на кнопке в нужном компоненте
+// внутрь ModalWindow вставляем нужную форму или что угодно по ситуации
+
+export const Modal = ({ isOpen, children, toggleModal }: TModal) => {
+  return isOpen ? (
+    <ModalContainer>
+      <ModalWindow>
+        <ModalCloseBtn onClick={toggleModal}/>
+        {children}
+      </ModalWindow>
+    </ModalContainer>
+  ) : null
+}
