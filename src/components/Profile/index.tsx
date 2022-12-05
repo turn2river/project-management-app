faebb2ab01dbce8077a85c685e7c8ad789b6af96
Alexey Krylov <72075841+turn2river@users.noreply.github.com>
2022-12-05 23:09:27 +0300

    feat: implement delete user in profile (#35)

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
 
 import { Button } from '../Button'
+
+import { auth } from '../../firebase_config'
+import { getAuth, deleteUser } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 import {
   ProfileBlock,
   PageContainer,
 } from './styled'
 
-import { auth } from '../../firebase_config'
-
 export const ProfilePage = () => {
 
-  // const [user, setUser] = useState([])
+  const navigate = useNavigate()
+
+  const onDeleteUser = () => {
+    const auth = getAuth()
+    const user = auth.currentUser
+
+    //@ts-expect-error aaa
+    deleteUser(user).then(() => {
+      navigate('/main', { replace: true })
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
 
   return (
     <PageContainer>
@@ -23,7 +36,7 @@ export const ProfilePage = () => {
           tasks: some tasks
       </ProfileBlock>
       <Button
-        handleClick={() => {}}
+        handleClick={() => {onDeleteUser()}}
         text='delete user'
         isDeleteButton
       />
